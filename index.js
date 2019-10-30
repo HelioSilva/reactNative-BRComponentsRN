db33df0420f9b0e52f21e08ff638416617e179d4
Helio Silva <suporte.computek@gmail.com>
2019-10-29 23:59:09 -0300

    Alteração do no Block para BrContent Fixes #1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import Block from './src/Block';
-import BrButton from './src/BrButton' ;
+import BrContent from './src/BrContent';
+import BrButton  from './src/BrButton' ;
 import TextField from './src/TextField' ;
 
-export {Block , BrButton , TextField }
+export {BrContent , BrButton , TextField }
